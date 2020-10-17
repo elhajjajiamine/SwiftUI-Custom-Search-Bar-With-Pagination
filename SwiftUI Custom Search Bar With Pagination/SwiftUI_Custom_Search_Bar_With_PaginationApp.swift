1ea5c4c815ca7de682c4d9cc9da478a7033c59d2
elhajjajiamine <aminselhajjaji@gmail.com>
2020-10-17 11:17:52 +0100

    Initial Commit

---
+++ SwiftUI Custom Search Bar With Pagination/SwiftUI_Custom_Search_Bar_With_PaginationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Custom_Search_Bar_With_PaginationApp.swift
+//  SwiftUI Custom Search Bar With Pagination
+//
+//  Created by elhajjaji on 10/17/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Custom_Search_Bar_With_PaginationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUI Custom Search Bar With PaginationTests/SwiftUI_Custom_Search_Bar_With_PaginationTests.swift
@@ -0,0 +1,33 @@
+//
+//  SwiftUI_Custom_Search_Bar_With_PaginationTests.swift
+//  SwiftUI Custom Search Bar With PaginationTests
+//
+//  Created by elhajjaji on 10/17/20.
+//
+
+import XCTest
+@testable import SwiftUI_Custom_Search_Bar_With_Pagination
+
+class SwiftUI_Custom_Search_Bar_With_PaginationTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
